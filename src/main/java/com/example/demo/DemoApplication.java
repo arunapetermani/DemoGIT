2580045059d7417f3ca6420e0253ed95dd51a50d
ARuna1430 <ArunaDeviKingstone@gmail.com>
2022-09-17 23:29:06 -0400

    done

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	//Demo by aruna
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
